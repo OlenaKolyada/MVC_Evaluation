4c0234d2683c97501349c940685f6ca61e30b675
OlenaKolyada <FunkyCorgi>
2024-08-16 19:42:23 +0200

    Saving genre works

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const selectElement = document.getElementById('genre');
     const form = document.getElementById('search_form');
 
+    const rootUrl = window.location.origin;
+    const currentUrl = window.location.href;
+
+    const homePagePath = '/FunkyEvaluation/public/';
+    const homePageUrl = `${rootUrl}${homePagePath}`;
+
+    if (currentUrl === homePageUrl || currentUrl === `${homePageUrl}index.php`) {
+        localStorage.removeItem('selectedGenre');
+        selectElement.value = '';
+        form.action = "?c=MovieController&m=getAllMovies";
+    } else {
+        const savedGenre = localStorage.getItem('selectedGenre');
+        if (savedGenre) {
+            selectElement.value = savedGenre;
+        } else {
+            selectElement.value = '';
+        }
+        form.action = selectElement.value === "" ? "?c=MovieController&m=getAllMovies" : "?c=MovieController&m=getMoviesByGenre";
+    }
+
     selectElement.addEventListener('change', function() {
         const genreSelect = this.value;
 
-        if (genreSelect === "") {
-            form.action = "?c=MovieController&m=getAllMovies";
-        } else {
-            form.action = "?c=MovieController&m=getMoviesByGenre";
+        try {
+            localStorage.setItem('selectedGenre', genreSelect);
+        } catch (e) {
+            console.error('Failed to save genre to localStorage:', e);
         }
-    });
 
-    if (selectElement.selectedIndex === 0) {
-        form.action = "?c=MovieController&m=getAllMovies";
-    }
+        form.action = genreSelect === "" ? "?c=MovieController&m=getAllMovies" : "?c=MovieController&m=getMoviesByGenre";
+    });
 });
